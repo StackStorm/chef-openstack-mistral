1b826e661c0bb42f3c0c95e46b4ba85a63e00231
Lindsay Hill <lindsay.k.hill@gmail.com>
2018-09-11 16:39:48 -0700

    standardized license field

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'openstack-mistral'
 maintainer 'StackStorm (st2-dev)'
 maintainer_email 'info@stackstorm.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures openstack-mistral'
 long_description 'Installs/Configures openstack-mistral'
 version '0.4.0'
